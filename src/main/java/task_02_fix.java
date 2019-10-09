8eaf2b999c6e401011346e28e614cb421b575b11
igorthedci <foumart@ukr.net>
2019-10-09 21:21:01 +0300

    Homework 07. Task 02 (completed)

--- src/main/java/task_02_fix.java
+++ src/main/java/task_02_fix.java
@@ -3,16 +3,21 @@ public class task_02_fix {
      * "2. Fix the method with try-catch to get the correct result of division:
      */
 
-    public static class Main {
+    public static void main(String[] args) {
+        System.out.println(divideByZero());
+    }
 
-        public static void main(String[] args) {
-            System.out.println(divideByZero());
-        }
+    public static int divideByZero() {
+        int a = 5;
+        int b = 0;
 
-        public static int divideByZero() {
-            int a = 5;
-            int b = 0;
+        try {
             return a / b;
+        }
+        catch (ArithmeticException eX) {
+            System.out.println("... catch section...");
+            return 0;
         }
     }
 }
+
